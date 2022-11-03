636e66ec71c95c8ad7533b398d296629993c035b
Määttä Eetu <eetuasseri@gmail.com>
2022-11-03 22:23:11 +0200

    FINAL PUSH FROM ME

--- Assets/Scripts/DoorLockScript.cs
+++ Assets/Scripts/DoorLockScript.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class DoorLockScript : MonoBehaviour
 {
+    public bool _doorLock;
+
+    [SerializeField] private Rigidbody door;
+    [SerializeField] private BoxCollider collider;
+
+    private RigidbodyConstraints locked = RigidbodyConstraints.FreezeAll;
+    private RigidbodyConstraints unLocked = RigidbodyConstraints.None;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(_doorLock == true)
+        {
+            door.constraints = locked;
+            collider.enabled = true;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(_doorLock == false)
+        {
+            door.constraints = unLocked;
+            collider.enabled = false;
+        }
     }
 }
